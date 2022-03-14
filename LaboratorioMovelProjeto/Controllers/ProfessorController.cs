367b6878bf58baee8b7f59ccc2140784bf941f94
Vitor Perez <vitorrperez@gmail.com>
2022-03-14 14:18:33 -0300

    Criação da entidade Professores

---
+++ LaboratorioMovelProjeto/Controllers/ProfessorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratorioMovelProjeto.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+        public IActionResult Conteudo()
+        {
+            return Content("Retornando o conteúdo em String");
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
