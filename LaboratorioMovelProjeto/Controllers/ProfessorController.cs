0f2f5a4cdfbea3fd171c5d9e6217ff920531f85d
Vitor Perez <vitorrperez@gmail.com>
2022-03-16 13:42:50 -0300

    Tela Editar

--- LaboratorioMovelProjeto/Controllers/ProfessorController.cs
+++ LaboratorioMovelProjeto/Controllers/ProfessorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LaboratorioMovelProjeto.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +9,44 @@ namespace LaboratorioMovelProjeto.Controllers
 {
     public class ProfessorController : Controller
     {
+
+        //Abrir tela Professores
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+
+        //Abrir Tela para inserir novo professor
+        [HttpGet]
         public IActionResult NovoProfessor()
         {
             return View();
         }
+
+
+        //Capturar dados do novo professor
+        [HttpPost]
+        public IActionResult NovoProfessor(ProfessorModel professorModel)
+        {
+            return View("Sucesso");
+        }
+
+        [HttpGet]
+        public IActionResult Editar()
+        {
+            return View("EditarProfessor");
+        }
+
 
         [HttpGet]
         public IActionResult Help()
         {
             return View();
         }
+
+        
 
         public IActionResult Conteudo()
         {
